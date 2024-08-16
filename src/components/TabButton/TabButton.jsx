6638bf7768d534b26b544655b6a8ae4cbc55c931
IvanJohnson571 <ivan_johnson2005@yahoo.com>
2024-08-16 11:06:27 +0300

    Split to small components logic.

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,15 +1,10 @@
 
-export default function TabButton({ children, onSelect, isSelected }) {
-
-    function handleClick() {
-        console.log("Hello World!");
-
-    };
+export default function TabButton({ children, isSelected, ...props }) {
 
     return (
         <>
             <li>
-                <button className={isSelected ? 'active' : ''} onClick={onSelect}>{children}</button>
+                <button className={isSelected ? 'active' : ''} {...props} >{children}</button>
             </li>
         </>
     )
